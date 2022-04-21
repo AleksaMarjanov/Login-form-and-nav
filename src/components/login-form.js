d6b1b15edac3bb468864049cede8e83d52889dd9
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-04-21 15:34:48 -0500

    deleted favicon, icons...

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,17 +1,17 @@
 import React from 'react'
+
 // Creating LoginForm that contains username and password input, h3 that says Login and a border.
 
 export default class LoginForm extends React.Component {
   render() {
     return (
-    <div className='container'>
         <div className='form-wrapper'>
-            <form  action="https://formsubmit.co/aleksa.marjanov@outlook.com" method="POST">
+            <form >
                 <h3>Log In</h3>
                 {/* Honeypot */}
                 <input type="text" name="_honey" style={{display: 'none' }}/>
                 <input type="hidden" name="_captcha" value="false"/>
-                <input type="hidden" name="_next" value="https://aleksamarjanov.github.io/Bootstrap-Website/success.html"/>
+                <input type="hidden" name="_next" value=""/>
             <div>   
                 <input required type='text' className='userName' name='userName' placeholder='Please enter username'/>
             </div>
@@ -19,14 +19,13 @@ export default class LoginForm extends React.Component {
                 <input required type='password' className='password' name='password' placeholder='Please enter password'/>
             </div>
             <div>
-            <div>
                 <input className='checkbox' type='checkbox'/> Remember Me
             </div>
+            <div>
                 <button type='submit' className='btn'>Login</button>
             </div>
             </form>
         </div>
-    </div>
   )
 }
 
